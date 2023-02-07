2c31df69e408bba4595a536eec13f17ef3828f05
Piotr Petryka <ppwxp19@gmail.com>
2023-02-07 11:18:16 +0100

    changing boot port

--- src/main/java/com/codecool/shop/ServingWebContentApplication.java
+++ src/main/java/com/codecool/shop/ServingWebContentApplication.java
@@ -2,9 +2,14 @@ package com.codecool.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class ServingWebContentApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServingWebContentApplication.class, args);
+        SpringApplication app = new SpringApplication(ServingWebContentApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8888"));
+        app.run(args);
     }
 }
