cd1c1149d1258cdb953f18b01f483957a32d30ca
stelmaszczykadrian <Tooyota12345@>
2023-02-08 15:21:54 +0100

    Added view of products, products by Category and by supplier.

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,15 +2,15 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Controller
@@ -18,15 +18,46 @@ public class ProductController {
 
     @GetMapping ( "/")
         public String index(Model model) {
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+
+        ProductService productService = new ProductService(productDataStore, productCategoryDataStore, supplierDataStore);
+
+        model.addAttribute("products", productService.getAllProducts());
+
+        return "index";
+
+    }
+
+
+    @GetMapping("/category/{id}")
+        public String category(@PathVariable("id") int id, Model model){
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+
+        ProductService productService = new ProductService(productDataStore, productCategoryDataStore, supplierDataStore);
+
+        model.addAttribute("category", productService.getProductCategory(id));
+        model.addAttribute("products", productService.getProductsForCategory(id));
+
+        return "category";
+    }
+
+
+    @GetMapping("/supplier/{id}")
+    public String supplier(@PathVariable("id") int id, Model model){
 
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
-        model.addAttribute("category", productService.getProductCategory(1));
-        model.addAttribute("products", productService.getProductsForCategory(1));
+        ProductService productService = new ProductService(productDataStore, productCategoryDataStore, supplierDataStore);
 
-            return "index";
+        model.addAttribute("products", productService.getSupplier(id).getProducts());
+
+        return "supplier";
     }
 }
 
--- src/main/java/com/codecool/shop/service/ProductService.java
+++ src/main/java/com/codecool/shop/service/ProductService.java
@@ -2,8 +2,10 @@ package com.codecool.shop.service;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 
 import java.util.List;
 
@@ -11,9 +13,12 @@ public class ProductService{
     private ProductDao productDao;
     private ProductCategoryDao productCategoryDao;
 
-    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao) {
+    private SupplierDao supplierDao;
+
+    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao, SupplierDao supplierDao) {
         this.productDao = productDao;
         this.productCategoryDao = productCategoryDao;
+        this.supplierDao = supplierDao;
     }
 
     public ProductCategory getProductCategory(int categoryId){
@@ -23,6 +28,23 @@ public class ProductService{
     public List<Product> getProductsForCategory(int categoryId){
         var category = productCategoryDao.find(categoryId);
         return productDao.getBy(category);
+    }
+
+    public Supplier getSupplier(int supplierId) {
+        return supplierDao.find(supplierId);
+    }
+
+    public List<ProductCategory> getAllCategories() {
+        return productCategoryDao.getAll();
+    }
+
+
+    public List<Supplier> getAllSuppliers() {
+        return supplierDao.getAll();
+    }
+
+    public List<Product> getAllProducts() {
+        return productDao.getAll();
     }
 
 
