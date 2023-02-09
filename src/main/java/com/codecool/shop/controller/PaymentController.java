46e283f02e7da611e3503bdfba4bae29d1778989
Piotr Petryka <ppwxp19@gmail.com>
2023-02-09 22:57:59 +0100

    Adding total price to payment template

--- src/main/java/com/codecool/shop/controller/PaymentController.java
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -1,12 +1,18 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.model.Cart;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PaymentController {
     @GetMapping( "/payment")
-    public String payment(){
+    public String payment(Model model, HttpSession session){
+        Cart cart = (Cart) session.getAttribute("cart");
+        model.addAttribute("totalPrice", cart.getTotalPrice());
+
         return "payment";
     }
 
