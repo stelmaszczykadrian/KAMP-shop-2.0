ab6b3f5ba8f77d86a535dd47ee6690ae75e53256
MonikaMarkulis <markulismonika@gmail.com>
2023-02-21 11:52:38 +0100

    add src/main/resources/connection.properties to gitignore

--- src/main/java/com/codecool/shop/dao/ProductCategoryDao.java
+++ src/main/java/com/codecool/shop/dao/ProductCategoryDao.java
@@ -10,7 +10,6 @@ public interface ProductCategoryDao {
     void add(ProductCategory category);
     ProductCategory find(int id);
     void remove(int id);
-
     List<ProductCategory> getAll();
 
 }
--- src/main/java/com/codecool/shop/dao/ProductDao.java
+++ src/main/java/com/codecool/shop/dao/ProductDao.java
@@ -12,7 +12,6 @@ public interface ProductDao {
     void add(Product product);
     Product find(int id);
     void remove(int id);
-
     List<Product> getAll();
     List<Product> getBy(Supplier supplier);
     List<Product> getBy(ProductCategory productCategory);
