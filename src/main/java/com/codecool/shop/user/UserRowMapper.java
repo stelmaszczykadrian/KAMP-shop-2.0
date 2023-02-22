8b75445a37f20cb987f2ef8349ec12d67a2b027f
Piotr Petryka <ppwxp19@gmail.com>
2023-02-22 13:58:42 +0100

    adding user row mapper

---
+++ src/main/java/com/codecool/shop/user/UserRowMapper.java
@@ -0,0 +1,18 @@
+package com.codecool.shop.user;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper implements RowMapper<User> {
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new User(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("email"),
+                rs.getString("password")
+        );
+    }
+}
