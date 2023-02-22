3be887e7dda052ebd7a18b9e95f5d65c9c7e4cbd
MonikaMarkulis <markulismonika@gmail.com>
2023-02-22 15:12:07 +0100

    add rowMappers

---
+++ src/main/java/com/codecool/shop/dao/jdbs/ProductCategoryDaoJbds.java
@@ -0,0 +1,38 @@
+package com.codecool.shop.dao.jdbs;
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductCategoryJbds implements ProductCategoryDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductCategoryJbds(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void add(ProductCategory category) {
+
+    }
+
+    @Override
+    public ProductCategory find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<ProductCategory> getAll() {
+        return null;
+    }
+}
---
+++ src/main/java/com/codecool/shop/dao/jdbs/ProductDaoJdbs.java
@@ -0,0 +1,2 @@
+package com.codecool.shop.dao.jdbs;public class ProductDaoJdbs {
+}
