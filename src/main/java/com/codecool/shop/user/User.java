d88ed6326309eafbc2587f6cd5083514968396d2
Piotr Petryka <ppwxp19@gmail.com>
2023-02-23 19:57:29 +0100

    adding hashing password to user object

--- src/main/java/com/codecool/shop/user/User.java
+++ src/main/java/com/codecool/shop/user/User.java
@@ -22,15 +22,20 @@ public class User {
     private String email;
     private Integer id;
 
+    public String getAuthority() {
+        return authority;
+    }
+
+    private String authority = "ROLE_USER";
+    private UserService userService = new UserService();
+
     public User(Integer id, String name, String email, String password) {
-        this.password = password;
+        if (password != null){
+            this.password = userService.passwordEncode(password);
+        }
         this.name = name;
         this.email = email;
         this.id = id;
-
-    }
-
-    public void isUserExist(String name, String password){
 
     }
 }
