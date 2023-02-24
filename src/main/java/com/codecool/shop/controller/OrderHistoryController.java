e6c9e044460d0dfccbda3f664d974cafb746272a
Piotr Petryka <ppwxp19@gmail.com>
2023-02-24 01:34:59 +0100

    orders now are saved to db after checkout

--- src/main/java/com/codecool/shop/controller/OrderHistoryController.java
+++ src/main/java/com/codecool/shop/controller/OrderHistoryController.java
@@ -1,32 +1,24 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.jdbc.OrderDao;
 import com.codecool.shop.model.Cart;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
-
 @AllArgsConstructor
 @Controller
-public class PaymentController {
+public class OrderHistoryController {
     private final JdbcTemplate jdbcTemplate;
 
-    @GetMapping( "/payment")
-    public String payment(Model model, HttpSession session, Principal principal){
-        OrderDao orderDao = new OrderDao(jdbcTemplate);
+    @GetMapping( "/orders")
+    public String orders(Model model, HttpSession session, Principal principal){
         Cart cart = (Cart) session.getAttribute("cart");
 
-        orderDao.save(cart, principal);
         model.addAttribute("totalPrice", cart.getTotalPrice());
-        return "payment";
+        return "orders";
     }
-
 }
